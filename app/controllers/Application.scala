74358ff5bb580d97b202e5cae0865bab64204feb
Andreas Boss <andreas.boss@finnova.com>
2016-08-24 07:29:24 +0200

    Add new route on / with redirect to api index

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import java.lang.ProcessBuilder.Redirect
+
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 
@@ -7,14 +9,25 @@ import play.api.mvc.{Action, Controller}
   * Created by Andreas Boss on 23.08.16.
   */
 class Application extends Controller {
+  /**
+    * Redirects to the api index
+    * @return
+    */
+  def index = Action {
+    Redirect(routes.Application.apiIndex())
+  }
 
-  def index = Action { implicit request =>
+  /**
+    * Entrypoint for the API
+    * @return
+    */
+  def apiIndex = Action { implicit request =>
     val json = Json.obj(
       "version" -> "0.1",
       "_links" -> Seq(
         Json.obj("rel" -> "self", "href" -> routes.Application.index().absoluteURL())
-        ,Json.obj("rel" -> "bricklets", "href" -> routes.Bricklets.getBricklets().absoluteURL())
-        ,Json.obj("rel" -> "door", "href" -> routes.Doors.getDoors().absoluteURL())
+        , Json.obj("rel" -> "bricklets", "href" -> routes.Bricklets.getBricklets().absoluteURL())
+        , Json.obj("rel" -> "door", "href" -> routes.Doors.getDoors().absoluteURL())
       )
     )
 
